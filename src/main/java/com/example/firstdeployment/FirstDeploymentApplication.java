2533a370bced12e3046d8cccad534287f4a940c5
breezyaloo123 <alassanesall771@gmail.com>
2021-05-18 16:42:26 +0000

    first deployment

---
+++ src/main/java/com/example/firstdeployment/FirstDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstDeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstDeploymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firstdeployment/FirstDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstdeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstDeploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
